81cba1ea3838f21c0e01ab2aaaf7f766839e0f50
Md. Mominur Rahman Sumon <81897668+Mominur-sumon@users.noreply.github.com>
2023-01-16 03:28:29 +0600

    CF_1549A solve in CPP

---
+++ CF_1549A.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t ;
+    cin >> t ;
+    while(t--){
+        int a ;
+        cin >> a;
+        cout << 2 <<" " <<(a / 2) * 2 << endl;
+    }
+
+    return 0 ;
+}
